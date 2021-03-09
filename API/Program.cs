23b0774c37d5eab6db1a76a82eaa3e0d8f977336
fahuamancaja <47445881+fahuamancaja@users.noreply.github.com>
2021-03-09 16:10:11 -0500

    Replaced Program.cs

--- API/Program.cs
+++ API/Program.cs
@@ -1,13 +1,6 @@
-using System;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using Serilog;
-using Serilog.Events;
-using TrailerDownloader.Context;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
-using System.Threading.Tasks;
 
 namespace TrailerDownloader
 {
@@ -16,25 +9,15 @@ namespace TrailerDownloader
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
-
-            // var host = CreateHostBuilder(args).Build();
-            // using var scope = host.Services.CreateScope();
-            // var services = scope.ServiceProvider;
-            // try
-            // {
-            //     var context = services.GetRequiredService<MovieDbContext>();
-            //     await context.Database.MigrateAsync();
-            // }
-            // catch (Exception ex)
-            // {
-            //     var logger = services.GetRequiredService<ILogger<Program>>();
-            //     logger.LogError(ex, "An error occurred during migration");
-            // }
-            // await host.RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.AddConsole();
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
